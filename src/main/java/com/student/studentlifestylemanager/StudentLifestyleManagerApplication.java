4d4675a09a74bbe6097881e81d12063e8e613736
Lalit Bhai ka Laptop <lalitd9675@gmail.com>
2025-06-23 20:22:51 +0530

    Initial commit - Spring Boot setup

--- src/main/java/com/student/studentlifestylemanager/StudentLifestyleManagerApplication.java
+++ src/main/java/com/student/studentlifestylemanager/StudentLifestyleManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentLifestyleManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentLifestyleManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentLifestyleManagerApplication.class, args);
+    }
 
 }
--- src/test/java/com/student/studentlifestylemanager/StudentLifestyleManagerApplicationTests.java
+++ src/test/java/com/student/studentlifestylemanager/StudentLifestyleManagerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentLifestyleManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
